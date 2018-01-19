e85eb6db285ac43354cbb87f6f006594f7daadd5
Dani Zaghian <dani.zaghian@gmail.com>
2018-01-18 19:31:33 -0800

    implemented restful index and show actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,13 @@
 class RecipesController < ApplicationController
 
-  def single_recipe_method
-    recipe = Recipe.first
-    render json: {title: recipe.title, chef: recipe.chef, ingredients: recipe.ingredients, instructions: recipe.directions}
+  def index
+    recipes = Recipe.all
+    render json: recipes.as_json
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    render json: recipe.as_json
   end
 
 end
