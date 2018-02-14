68d9caee40ac7accdb827497430581bfe35e6abc
Dani Zaghian <dani.zaghian@gmail.com>
2018-02-13 18:24:00 -0800

    using template and js to create repeating html

---
+++ public/index.js
@@ -0,0 +1,9 @@
+var recipeTemplate = document.querySelector("#recipe-card");
+var recipeRow = document.querySelector(".row");
+
+recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
+recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
